dac5add74d6925531628058deb0055db81ebbc43
gdmec07150835 <853671221@qq.com>
2017-01-02 22:28:52 +0800

    Initial commit

---
+++ app/src/main/java/a07150835/gdmec/com/mymap/com/baidu/mapapi/clusterutil/clustering/Cluster.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2015 Baidu, Inc. All Rights Reserved.
+ *//*
+
+
+package a07150835.gdmec.com.mymap.com.baidu.mapapi.clusterutil.clustering;
+
+
+import com.baidu.mapapi.model.LatLng;
+
+import java.util.Collection;
+
+*/
+/**
+ * A collection of ClusterItems that are nearby each other.
+ *//*
+
+public interface Cluster<T extends com.baidu.mapapi.clusterutil.clustering.ClusterItem> {
+    public LatLng getPosition();
+
+    Collection<T> getItems();
+
+    int getSize();
+}*/
